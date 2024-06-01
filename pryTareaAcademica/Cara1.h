f5ffb6442b5c82caa4dbe98df483fca46379b13b
albeto9 <albpg@DESKTOP-FAOHBOD>
2024-06-01 16:20:11 -0500

    Eliminando redundancias

--- pryTareaAcademica/Cara1.h
+++ pryTareaAcademica/Cara1.h
@@ -8,6 +8,9 @@ public:
 	void mostrar(Graphics^ g) {
 		g->DrawEllipse(Pens::Blue, x, y, ancho, alto);
 		//DIBUJO SOLO DE LA BOCA
+		Pen^ lapicero = gcnew Pen(Color::Aquamarine, 5);
+		lapicero->LineJoin = Drawing2D::LineJoin::Miter;
+		g->DrawLine(lapicero, 290, 180, 330, 180); 
 	}
 	void mostrarSeleccion(Graphics^ g) {
 		mostrarSeleccionCentro();
