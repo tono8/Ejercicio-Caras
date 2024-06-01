74b8bf4cbd067841fccf6082ef06b84bad838edb
albeto9 <albpg@DESKTOP-FAOHBOD>
2024-06-01 01:11:36 -0500

    Cambios en el formulario principal (Menu principal)

--- pryTareaAcademica/frmPrincipal.h
+++ pryTareaAcademica/frmPrincipal.h
@@ -34,6 +34,10 @@ namespace pryTareaAcademica {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
 
 	private:
 		/// <summary>
@@ -48,11 +52,53 @@ namespace pryTareaAcademica {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"frmPrincipal";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(269, 63);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(160, 22);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"MENU PRINCIPAL";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(185, 185);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(130, 60);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Inicio de carrera";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(370, 185);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(130, 60);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"Cambio de caritas";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// frmPrincipal
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(684, 441);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Name = L"frmPrincipal";
+			this->Text = L"frmPrincipal";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
