4cc4e67055f8c5b1fec2b9bad8ab6ac2eaa0c747
albeto9 <albpg@DESKTOP-FAOHBOD>
2024-06-02 19:17:28 -0500

    revirtiendo de float a int en la variable dx

--- pryTareaAcademica/Cara.h
+++ pryTareaAcademica/Cara.h
@@ -5,14 +5,15 @@ class CaraBase //Se establece el tamaño y posicion de los ojos debido a que se r
 protected:
 	int x, y;
 	int ancho, alto;
-	float dx;
+	int dx;
 	int color;
 public:
 	CaraBase() {
 		x = 20;
 		alto = ancho = 160;//dependiendo del diseño elegido se cambia este parametro
 		System::Random v;
-		dx = v.Next(2.0, 16.0);//La carrera no va a terminar si dos o tres tienen la misma velocidad ## - Esto no esta en la rubrica de la tarea - ##
+		dx = v.Next(2, 16);
+		//La carrera no va a terminar si dos o tres tienen la misma velocidad ## - Esto no esta en la rubrica de la tarea - ##
 		System::Threading::Thread::Sleep(10);
 	};
 	~CaraBase() {};
